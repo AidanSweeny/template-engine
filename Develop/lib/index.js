b6c68e27404da0afe2b3cc5916957bda4bcefa5e
Aidan Sweeny <apswee22@colby.edu>
2020-07-15 18:37:37 -0400

    Added working prompts

--- Develop/lib/index.js
+++ Develop/lib/index.js
@@ -4,53 +4,55 @@ const manager = require("../lib/Manager");
 const intern = require("../lib/Intern");
 const engineer = require("../lib/Engineer");
 const inquirer = require("inquirer");
-// const eng1 = new engineer("Aidan", 3, "aidansweeny@gmail.com", "AidanSweeny");
-// const eng2 = new engineer("Aidan", 3, "aidansweeny@gmail.com", "AidanSweeny");
-// const eng3 = new engineer("Aidan", 3, "aidansweeny@gmail.com", "AidanSweeny");
-// const employees = [eng1, eng2, eng3];
-var prompt = inquirer.createPromptModule();
-while (stillPeople){
-    prompt([{
-        type: "input",
-        message: "What is your name?",
-        name: "name"
-      },{
-        type: "input",
-        message: "What is your ID?",
-        name: "id"
-      },{
-        type: "input",
-        message: "What is your email?",
-        name: "email",
-      }])
-      .then(function({name, id, email}) {
-        
-      })
-}
-prompt([{
-    type: "input",
-    message: "What is your project title?",
-    name: "title"
-  },{
-    type: "input",
-    message: "What is a description of the project?",
-    name: "description"
-  },{
-    type: "checkbox",
-    message: "What button would you like",
-    name: "button",
-    choices: ["Website up", "Website down", "Uses CSS", "Uses HTML", "Uses Javascript"]
-  },{
-    type: "input",
-    message: "How and what is required to install this project?",
-    name: "installation"
-  }])
-  .then(async function({title}) {
-        try {
+let employees = [];
 
-        }
-        catch(err) {
-          console.log(err);
-        }
-  })
-console.log(render(employees, employee, manager, intern, engineer));
+function prompted(){   
+    inquirer
+        .prompt([{
+            type: "input",
+            message: "What is your name?",
+            name: "theName"
+        },{
+            type: "input",
+            message: "What is your profession?",
+            name: "job"
+        },{
+            type: "input",
+            message: "What is your ID?",
+            name: "id"
+        },{
+            type: "input",
+            message: "What is your email?",
+            name: "email",
+        }])
+        .then(async function({theName, job, id, email}) {
+            if(job.toLowerCase() === "engineer"){
+                await inquirer.prompt([{type: "input", message: "What is your Github Username?", name: "username"}]).then(function({username}){
+                    employees.push(new engineer(theName, id, email, username))
+                })
+            }
+            else if(job.toLowerCase() === "intern"){
+                await inquirer.prompt([{type: "input", message: "What school do you go to?", name: "school"}]).then(function({school}){
+                    employees.push(new intern(theName, id, email, school))
+                })
+            }
+            else if(job.toLowerCase() === "manager"){
+                await inquirer.prompt([{type: "input", message: "What is your office number?", name: "office"}]).then(function({office}){
+                    employees.push(new manager(theName, id, email, office))
+                })
+            }
+            inquirer.prompt([{
+                type: "confirm",
+                message: "Would you like to add more people?",
+                name: "people"
+            }]).then(function({people}){
+                if(people){
+                    prompted();
+                }
+                else {
+                    fs.writeFile(render(employees));
+                }
+            })
+        })
+}
+prompted();
